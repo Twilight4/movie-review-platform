819f4e003ae6b3706c6bd55d37f939fd046eab15
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-12-11 20:08:11 +0100

    adding outputs.tf

---
+++ terraform/modules/gke/outputs.tf
@@ -0,0 +1,44 @@
+output "cluster_name" {
+  description = "The name of the GKE cluster."
+  value       = google_container_cluster.gke.name
+}
+
+output "cluster_id" {
+  description = "The full resource ID of the GKE cluster."
+  value       = google_container_cluster.gke.id
+}
+
+output "location" {
+  description = "The region or zone where the cluster is deployed."
+  value       = google_container_cluster.gke.location
+}
+
+output "endpoint" {
+  description = "The IP address of the GKE master endpoint."
+  value       = google_container_cluster.gke.endpoint
+}
+
+output "node_pool_names" {
+  description = "List of names of the created node pools."
+  value       = google_container_node_pool.gke_pool[*].name
+}
+
+output "node_pool_ids" {
+  description = "List of full resource IDs for the node pools."
+  value       = google_container_node_pool.gke_pool[*].id
+}
+
+output "network" {
+  description = "The VPC network used by the cluster."
+  value       = google_container_cluster.gke.network
+}
+
+output "subnetwork" {
+  description = "The subnetwork used by the cluster."
+  value       = google_container_cluster.gke.subnetwork
+}
+
+output "service_account" {
+  description = "The service account used by the cluster nodes."
+  value       = google_service_account.gke_nodes.email)
+}
