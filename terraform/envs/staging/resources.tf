894beeaa39b89f09d2d3fd85f551383c1aecb639
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-12-11 20:14:09 +0100

    removing project argument from resources.tf

--- terraform/envs/staging/resources.tf
+++ terraform/envs/staging/resources.tf
@@ -1,7 +1,5 @@
 # Enable necessary APIs
 resource "google_project_service" "required_apis" {
-  project = var.project_id
-
   for_each = toset([
     "compute.googleapis.com",
     "container.googleapis.com",
@@ -20,14 +18,12 @@ resource "google_project_service" "required_apis" {
 
 # VPC
 resource "google_compute_network" "gke_vpc" {
-  project                 = var.project_id
   name                    = "${var.prefix}-gke-vpc"
   auto_create_subnetworks = false
 }
 
 # Subnet
 resource "google_compute_subnetwork" "gke_subnet" {
-  project       = var.project_id
   name          = "${var.prefix}-gke-subnet"
   ip_cidr_range = "10.10.0.0/20"
   network       = google_compute_network.gke_vpc.id
@@ -36,7 +32,6 @@ resource "google_compute_subnetwork" "gke_subnet" {
 
 # Firewall rules: allow internal cluster communication
 resource "google_compute_firewall" "gke_internal" {
-  project = var.project_id
   name    = "${var.prefix}-gke-internal"
   network = google_compute_network.gke_vpc.name
 
