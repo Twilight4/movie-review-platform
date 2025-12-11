e8b7b0c219be54d23e03be8046b602004d1ed028
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-12-11 18:49:16 +0100

    update terraform config

--- terraform/envs/staging/main.tf
+++ terraform/envs/staging/main.tf
@@ -1,3 +1,11 @@
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+module "gke" {
+  source = "../../modules/gke"
+
+  cluster_name = "staging-gke"
+  region       = var.region
+
+  network           = google_compute_network.gke_vpc.self_link
+  subnetwork        = google_compute_subnetwork.gke_subnet.self_link
+  node_count        = 3
+  node_machine_type = "e2-small"
 }
--- terraform/envs/staging/terraform.tfvars
+++ terraform/envs/staging/terraform.tfvars
@@ -1,4 +1,4 @@
-prefix  = "stg"
-project = "movie-review-platform8451"
-region  = "europe-central2"
-zone    = "europe-central2-c"
+prefix     = "stg"
+project_id = "movie-review-platform8451"
+region     = "europe-central2"
+zone       = "europe-central2-c"
