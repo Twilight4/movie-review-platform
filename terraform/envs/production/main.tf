0ce1f76412176f089029efe033db4b8d6fd9c8fb
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-12-18 19:07:07 +0100

    adding firestore modules to production env

--- terraform/envs/production/main.tf
+++ terraform/envs/production/main.tf
@@ -11,3 +11,26 @@ module "gke" {
   disk_type         = "pd-standard"
   disk_size_gb      = 30
 }
+
+module "firestore" {
+  source = "../../modules/firestore/"
+
+  project_id = "movie-review-platform8451"
+  region     = var.region
+
+  database_name           = "firestore"
+  type                    = "FIRESTORE_NATIVE"
+  deletion_policy         = "DELETE"
+  delete_protection_state = "DELETE_PROTECTION_DISABLED"
+}
+
+module "iam" {
+  source = "../../modules/iam/"
+}
+
+module "workloads" {
+  source = "../../modules/workloads/"
+
+  project_id               = "movie-review-platform8451"
+  gcp_service_account_name = module.iam.service_account_name
+}
