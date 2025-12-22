bfdb7c20dfb9d55ae34bbf14108fe0066b913cc7
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-12-22 22:47:34 +0100

    adding more iam roles

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -9,3 +9,32 @@ resource "google_project_iam_member" "firestore_access" {
   member  = "serviceAccount:${google_service_account.movie_api.email}"
 }
 
+resource "google_project_iam_member" "gke_node_lb_admin" {
+  project = var.project_id
+  role    = "roles/compute.loadBalancerAdmin"
+  member  = "serviceAccount:${google_service_account.movie_api.email}"
+}
+
+resource "google_project_iam_member" "gke_node_sa" {
+  project = var.project_id
+  role    = "roles/container.nodeServiceAccount"
+  member  = "serviceAccount:${google_service_account.movie_api.email}"
+}
+
+resource "google_project_iam_member" "gke_node_computer_viewer" {
+  project = var.project_id
+  role    = "roles/compute.viewer"
+  member  = "serviceAccount:${google_service_account.movie_api.email}"
+}
+
+resource "google_project_iam_member" "gke_node_compute_networkadmin" {
+  project = var.project_id
+  role    = "roles/compute.networkAdmin"
+  member  = "serviceAccount:${google_service_account.movie_api.email}"
+}
+
+resource "google_project_iam_member" "gke_node_compute_securityadmin" {
+  project = var.project_id
+  role    = "roles/compute.securityAdmin"
+  member  = "serviceAccount:${google_service_account.movie_api.email}"
+}
